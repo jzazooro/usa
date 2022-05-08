c55ed3aa046a25dda47549e4cd3577173fd722e0
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-05-08 12:54:36 +0200

    funcion que haga los graficos

--- clases/codigograficos.py
+++ clases/codigograficos.py
@@ -8,4 +8,13 @@ class codigografico:
         self.archivo=archivo
         self.dt=pd.read_csv(archivo)
     
-    
+    def grafico(self, tipodegrafico, columnauno, columnados):
+        
+        def pearson():
+            correlacion=np.corrcoef(self.dt[columnauno], self.dt[columnados])
+            correlacion=round(correlacion[0], [1], 3)
+            return correlacion
+        
+        print("el coeficiente de correlacion de pearson es {}".format(pearson()))
+        fig, ax =plt.subplots()
+        
